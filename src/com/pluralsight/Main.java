7049c1fee4d75d05fe3be7dd2a0098bfd9691969
David Ström <david.strom88@gmail.com>
2022-03-31 15:46:34 +0200

    First commit

---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i = 3;
+
+        System.out.printf("Hello IntelliJ CE %d%n", i);
+    }
+
+    public static int getDistance(int time, int velocity) {
+        return velocity * time;
+    }
+}
+
